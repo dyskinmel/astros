0eef044a71db29400bdbf8c0f2d8a469b883893e
dyskinmel <47494257+dyskinmel@users.noreply.github.com>
2023-11-16 16:24:17 +0000

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,15 @@ module.exports = {
 			fontFamily: {
 				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
+			colors:{
+				'brand-primary':'#0097b2',
+				'brand-secondary' : '#FF7043',
+				'brand-tertiary' : '#00BCD4',
+				'brand-success' : '#66BB6A',
+				'brand-warning' : '#FFA726',
+				'brand-error' : '#EF5350',
+				'brand-surface' : '#FAFAFA',
+			},
 		},
 	},
 	plugins: [require("@tailwindcss/typography"), require.resolve("prettier-plugin-astro")],
